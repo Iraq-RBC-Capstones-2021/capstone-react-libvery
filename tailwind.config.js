8b03084c99266fdc2ed2dfcd2846782d21140852
Zahraa <alwarda@hotmail.co.uk>
2021-10-06 09:37:22 -0700

    setup(tailwind): customize the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+// const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: "#F2E1D9",
+      secondary: "#BE856A",
+      black: "#3F3B3B",
+      Cards: "#F9F3EE",
+    },
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      serif: ["Roboto", "serif"],
+    },
     extend: {},
   },
   variants: {
