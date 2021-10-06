d37e1e13e4ebcc7d304cd6f321a9f2883065252b
RawandDev <rawand.200091384037@gmail.com>
2021-10-06 09:05:42 +0300

    setup(firebase): setup authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDPJiCx_MN8oHqQUPVLqia14yvfgZqgV2U",
@@ -14,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db, auth };
