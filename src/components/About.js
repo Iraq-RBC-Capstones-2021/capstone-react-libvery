06fad24f0e67566951ea395d829ff25e82ff8655
RawandDev <rawand.200091384037@gmail.com>
2021-10-07 14:22:48 +0300

    setup(about): add about component

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="bg-primary h-screen font-sans">
+      <div className="text-center">
+        <h1 className="text-2xl font-semibold">Our Story</h1>
+        <p>
+          We are a team of developers who love to create and build things. We
+          build Libvery to help you easily find the books you need.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
