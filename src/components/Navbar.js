0d9ce019cfa674e94f4927cc3f6de5e10a26ea15
RawandDev <rawand.200091384037@gmail.com>
2021-10-07 14:54:29 +0300

    feat(demo): add demo link to showcase the use of routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ABOUT_ROUTE, FAVOURITES_ROUTE, HOME_ROUTE } from "../routes";
 
 function Navbar() {
   return (
     <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/favourites">Favourites</Link>
-      <Link to="/favourites">Favourites</Link>
+      <Link to={HOME_ROUTE}>Home</Link>
+      <Link to={ABOUT_ROUTE}>About</Link>
+      <Link to={FAVOURITES_ROUTE}>Favourites</Link>
     </div>
   );
 }
