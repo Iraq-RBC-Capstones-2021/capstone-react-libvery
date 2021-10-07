b69bffddb6faf576c30095601730bbf1ea2bb43d
RawandDev <rawand.200091384037@gmail.com>
2021-10-07 15:44:37 +0300

    feat(eye-catch): make the libvery name stand out

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,12 @@ function About() {
         <h1 className="text-2xl font-semibold mb-3">Our Story</h1>
         <p>
           We are a team of developers who love to create and build things. We
-          built Libvery to help you easily find the books you need. Our aim is
-          to make life much more easier to access books and make a better
-          generation.
+          built{" "}
+          <span className="transition ease-linear font-semibold font-sans border-b-2 border-transparent hover:border-current cursor-pointer text-secondary">
+            Libvery
+          </span>{" "}
+          to help you easily find the books you need. Our aim is to make life
+          much more easier to access books and make a better generation.
         </p>
       </div>
     </div>
