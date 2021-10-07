c8bff322bc47850ed4ebdba2894148dc46a78a2c
RawandDev <rawand.200091384037@gmail.com>
2021-10-07 15:31:19 +0300

    style(our-story): add our story with styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,13 @@ import React from "react";
 function About() {
   return (
     <div className="bg-primary h-screen font-sans">
-      <div className="text-center">
-        <h1 className="text-2xl font-semibold">Our Story</h1>
+      <div className="text-center mx-10 sm:mx-32 lg:mx-96">
+        <h1 className="text-2xl font-semibold mb-3">Our Story</h1>
         <p>
           We are a team of developers who love to create and build things. We
-          build Libvery to help you easily find the books you need.
+          built Libvery to help you easily find the books you need. Our aim is
+          to make life much more easier to access books and make a better
+          generation.
         </p>
       </div>
     </div>
