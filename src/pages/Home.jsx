143a85e52d8a78651cf27ee0c4254dda61cf8d1b
RawandDev <rawand.200091384037@gmail.com>
2021-10-08 09:38:44 +0300

    fix(deployment): fix deployment issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,11 @@
-export default function Home(props) {
-    
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 }
+
+export default Home;
