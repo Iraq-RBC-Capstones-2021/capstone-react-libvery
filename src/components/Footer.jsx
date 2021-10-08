b449a07168efa1dc72583be8bce1b78feed13324
Niyaz Irfan <niyaz.irfan119@gmail.com>
2021-10-08 18:42:07 +0300

    add temperory links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import projectLogo from "../assets/projectLogo.svg";
 import { FaFacebook, FaInstagram, FaTwitter, FaGithub } from "react-icons/fa";
 import { GiCheckMark } from "react-icons/gi";
+import { Link } from "react-router-dom";
+import {
+  HOME_ROUTE,
+  ABOUT_ROUTE,
+  BOOKS_ROUTE,
+  FAVOURITES_ROUTE,
+} from "../routes";
 
 function Footer() {
   return (
@@ -25,18 +32,18 @@ function Footer() {
             </div>
             <div className="flex flex-col">
               <div className="flex justify-center mt-2">
-                <a href="/" className="mx-5">
+                <Link to={HOME_ROUTE} className="mx-5">
                   Home
-                </a>
-                <a href="/about" className="mx-5">
+                </Link>
+                <Link to={ABOUT_ROUTE} className="mx-5">
                   About
-                </a>
-                <a href="/books" className="mx-5">
+                </Link>
+                <Link to={BOOKS_ROUTE} className="mx-5">
                   Books
-                </a>
-                <a href="/favorites" className="mx-5">
+                </Link>
+                <Link to={FAVOURITES_ROUTE} className="mx-5">
                   Favorites
-                </a>
+                </Link>
               </div>
               <div className="flex flex-col justify-end items-center mt-10 md:mt-16 mb-5">
                 <p className=" pb-1 md:mr-24 ">Never miss an update</p>
@@ -55,14 +62,22 @@ function Footer() {
                 </div>
               </div>
             </div>
-            <div className="mb-5 md:col-span-2 lg:col-span-1">
+            <div className="mb-5 md:col-span-2 lg:col-span-1 flex flex-col">
               <div className="flex justify-around  mb-5 mt-2 lg:mb-6">
-                <FaFacebook size="1.7rem" />
-                <FaInstagram size="1.7rem" />
-                <FaTwitter size="1.7rem" />
-                <FaGithub size="1.7rem" />
+                <a href="/facebook" target="_blank">
+                  <FaFacebook size="1.7rem" />
+                </a>
+                <a href="/instagram" target="_blank">
+                  <FaInstagram size="1.7rem" />
+                </a>
+                <a href="/twitter" target="_blank">
+                  <FaTwitter size="1.7rem" />
+                </a>
+                <a href="/github" target="_blank">
+                  <FaGithub size="1.7rem" />
+                </a>
               </div>
-              <select className="mt-5 lg:mt-16" id="lang-dropdown">
+              <select className="my-5 lg:mt-16 self-center" id="lang-dropdown">
                 <option value="english">EN</option>
                 <option value="arabic">AR</option>
                 <option value="arabic">KU</option>
