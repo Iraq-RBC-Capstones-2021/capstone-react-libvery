83bb3ec60bff1ed3112e778a8ea0dcef452babef
RawandDev <rawand.200091384037@gmail.com>
2021-10-08 14:32:18 +0300

    fix(deployment): fix deployment issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,11 @@
-export default function Navbar(props) {
-    
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
 }
+
+export default Navbar;
