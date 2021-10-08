61c4db95f6088313d53d12ddfb6cdb809f4a6103
RawandDev <rawand.200091384037@gmail.com>
2021-10-08 22:14:09 +0300

    style(mobile): add style for mobile

--- src/components/BooksDetail.jsx
+++ src/components/BooksDetail.jsx
@@ -1,9 +1,63 @@
 import React from "react";
+import book from "./book.jpg";
 
 function BooksDetail() {
   return (
-    <div>
-      <h1>Books Detail</h1>
+    <div className="bg-primary h-screen font-sans sm:grid">
+      <div className="relative">
+        <img className="" src={book} alt="book" />
+        <div className="drop-shadow-xl absolute bottom-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-10 w-10"
+            viewBox="0 0 20 20"
+            fill="#b6b6b6"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-3">
+        <h1 className="font-semibold">Book Title</h1>
+        <button
+          type="button"
+          className="bg-secondary text-white rounded-sm px-2 py-1 col-start-3"
+        >
+          Contact Seller
+        </button>
+        <p>
+          Author: <span>author name</span>
+        </p>
+        <button
+          type="button"
+          className="bg-secondary text-white rounded-sm px-2 py-1 col-start-3"
+        >
+          Edit
+        </button>
+        <p>
+          Genre:{" "}
+          <span className="bg-black text-white rounded-sm cursor-pointer py-1">
+            Action
+          </span>{" "}
+          -{" "}
+          <span className="bg-black text-white rounded-sm cursor-pointer py-1">
+            Adventure
+          </span>
+        </p>
+        <p className="col-start-3">
+          Price: <span>3.99$</span>
+        </p>
+        <p className="col-span-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam esse
+          cupiditate amet dolor quasi eius, itaque fuga, adipisci magnam vel
+          cumque? Ex cupiditate inventore obcaecati doloremque expedita iusto
+          labore ipsam?
+        </p>
+      </div>
     </div>
   );
 }
