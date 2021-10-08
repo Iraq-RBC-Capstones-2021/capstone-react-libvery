5aba9f49f072fe6cd868d73bc4b15f068d7402d4
RawandDev <rawand.200091384037@gmail.com>
2021-10-08 16:14:48 +0300

    update(error): update error page with some animation

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import error from "../assets/error.svg";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { HOME_ROUTE } from "../routes";
 
 function Error() {
   return (
@@ -7,12 +10,16 @@ function Error() {
       <h1 className="text-4xl font-semibold mr-28 text-center">Ooops,</h1>
       <p className="text-xl text-center">There is no such a page</p>
       <img className="mx-auto sm:max-w-xl" src={error} alt="error" />
-      <button
-        type="button"
-        className="bg-secondary text-Cards px-3 py-1 rounded-md mx-auto mt-8 text-2xl transform bg-opacity-90 hover:bg-opacity-100 focus:outline-none focus:ring-4 focus:ring-secondary focus:ring-opacity-50"
-      >
-        Back
-      </button>
+      <Link to={HOME_ROUTE} className="text-center">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          transition={{ duration: 0.2 }}
+          className="bg-secondary text-Cards px-3 py-1 rounded-md mx-auto mt-8 text-2xl transform bg-opacity-90 hover:bg-opacity-100 focus:outline-none focus:ring-4 focus:ring-secondary focus:ring-opacity-50"
+        >
+          Back
+        </motion.button>
+      </Link>
     </div>
   );
 }
