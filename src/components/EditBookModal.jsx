9370f903fe797a8d62be9f92b54c94bba480dd34
RawandDev <rawand.200091384037@gmail.com>
2021-10-10 18:03:35 +0300

    modal(edit-book): create edit book modal

--- src/components/EditBookModal.jsx
+++ src/components/EditBookModal.jsx
@@ -12,12 +12,22 @@ function ContactModal({ isEditBookOpen, setIsEditBookOpen }) {
     <>
       {isEditBookOpen && (
         <Modal
-          className="bg-black mx-auto mt-40 rounded-lg text-white h-2/4 w-3/4 sm:max-w-sm"
+          //   className="bg-green-400"
           isOpen={isEditBookOpen}
           onRequestClose={() => setIsEditBookOpen(false)}
           style={{
             overlay: {
               backgroundColor: "rgba(0, 0, 0, 0.5)",
+            },
+            content: {
+              top: "50%",
+              left: "50%",
+              right: "auto",
+              bottom: "auto",
+              transform: "translate(-50%, -50%)",
+              border: "none",
+              borderRadius: "0",
+              backgroundColor: "transparent",
             },
           }}
         >
@@ -30,17 +40,77 @@ function ContactModal({ isEditBookOpen, setIsEditBookOpen }) {
                 duration: 0.5,
               },
             }}
-            className="flex flex-col justify-center items-center text-white sm:max-w-sm mx-auto relative"
+            className="flex flex-col justify-center items-center text-white sm:max-w-2xl bg-white rounded-lg p-10 mx-auto relative"
           >
             <CloseButton
               setIsEditBookOpen={setIsEditBookOpen}
               isEditBookOpen={isEditBookOpen}
             />
-            <img
-              src={defaultImage}
-              alt="default"
-              className="w-32 mx-auto mt-8"
-            />
+            <form className="w-full max-w-md bg-white rounded mb-4 mt-10 sm:max-w-2xl md:max-w-md">
+              <h2 className="text-black font-semibold flex justify-center mb-4 text-2xl">
+                Update Book Details
+              </h2>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
+                id="bookTitle"
+                type="text"
+                placeholder="Book Title *"
+              />
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
+                id="author"
+                type="text"
+                placeholder="Author *"
+              />
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
+                id="category"
+                type="text"
+                placeholder="Book Category *"
+              />
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
+                id="price"
+                type="text"
+                placeholder="Price *"
+              />
+              <textarea
+                name="decription"
+                id="decription"
+                cols="22"
+                rows="5"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
+                placeholder="Description"
+              ></textarea>
+              <label className="w-60 text-white flex items-center py-1 rounded-md cursor-pointer bg-secondary bg-opacity-50 hover:bg-secondary justify-center">
+                <span>Upload Book Cover</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
+                  />
+                </svg>
+                <input type="file" className="hidden" />
+              </label>
+              <label className="flex text-black items-center">
+                <p className="mr-2">Are you donating this book?</p>
+                <input type="checkbox" name="yes" id="yes" />
+              </label>
+              <button
+                className="bg-secondary text-white font-semibold py-1 px-4 rounded-sm mb-4 container mt-2"
+                type="submit"
+              >
+                Update Book
+              </button>
+            </form>
           </motion.div>
         </Modal>
       )}
