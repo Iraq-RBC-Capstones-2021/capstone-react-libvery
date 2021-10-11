9a65735f2fb72280230015541b1268b46f45d88c
Zahraa <alwarda@hotmail.co.uk>
2021-10-10 23:13:33 -0700

    Revert "add translation to auth pages"
    This reverts commit ee36e5eae038b38710f36cb7a4a496ef5db168f4.

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useTranslation } from "react-i18next";
 
 import Google from "../assets/Google.svg";
 import Email from "../assets/Email.svg";
@@ -8,7 +7,7 @@ import SigninIllustration from "../assets/SigninIllustration.svg";
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const { t } = useTranslation();
+
   const handleShowPassword = (e) => {
     e.preventDefault();
     setShowPassword(!showPassword);
@@ -18,7 +17,7 @@ const SignIn = () => {
     <div className="min-h-screen flex  items-center justify-around flex-wrap  bg-primary font-sans ">
       <div className="bg-white m-2 p-10 md:p-16 rounded-xl md:w-6/12 lg:w-5/12 ">
         <h1 className="text-4xl font-bold font-sans	 mb-10 text-black">
-          {t("sign_in")} 📚
+          Log In 📚
         </h1>
 
         <form className="space-y-5">
@@ -35,7 +34,7 @@ const SignIn = () => {
                         border-gray-200 p-3 rounded outline-none  focus:border-black placeholder-gray-400
                         form-input border  py-3 px-4  appearance-none  block pl-14 focus:outline-none
                         "
-              placeholder={t("email")}
+              placeholder="E-mail"
             />
           </div>
 
@@ -51,26 +50,26 @@ const SignIn = () => {
                         border-gray-200 p-3 rounded outline-none focus:border-black placeholder-gray-400
                         form-input border  py-3 px-4 bg-white   appearance-none  block pl-14 focus:outline-none
                         "
-              placeholder={t("password")}
+              placeholder="Password"
             />
             <button
               className="cursor-pointer h-6 w-6 absolute top-1/2 transform -translate-y-1/2 right-6 font-light text-xs text-gray-600"
               onClick={handleShowPassword}
             >
-              {!showPassword ? `${t("show")}` : `${t("hide")}`}
+              {!showPassword ? "Show" : "Hide"}
             </button>
           </div>
 
           <button className="block w-full bg-secondary p-4 rounded text-white font-bold	 hover:text-black transition duration-300">
-            {t("sign_in_button")}
+            Sign In
           </button>
 
           <div className="flex items-center  text-gray-800  font-light text-sm">
-            {t("don't_have_an_account")}?
-            <span className="pl-2  font-bold"> {t("sign_up")}! </span>
+            Don't have an account?{" "}
+            <span className="pl-2  font-bold"> Sign up </span>
           </div>
 
-          <p className="text-center text-gray-500 font-light">- {t("or")} -</p>
+          <p className="text-center text-gray-500 font-light">- OR -</p>
 
           <div className="flex items-center justify-center space-x-4 text-gray-800  ">
             <img src={Google} alt="" />
