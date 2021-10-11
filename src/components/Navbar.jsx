0f6036a2cfc0b5676a0281ced28bd90ef0dc8dbc
ahmed <a.alrahoo2001@gmail.com>
2021-10-11 18:22:51 +0300

    feat(navbar):adding the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-
+import { Link } from "react-router-dom";
+import {
+  HOME_ROUTE,
+  BOOKS_ROUTE,
+  ABOUT_ROUTE,
+  FAVOURITES_ROUTE,
+} from "../routes";
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -16,21 +22,21 @@ function Navbar() {
           id="my-drawer-3"
           type="checkbox"
           onChange={() => setIsOpen(!isOpen)}
-          class="drawer-toggle"
+          className="drawer-toggle"
         />
-        <div class="flex flex-col drawer-content">
-          <div class="w-full navbar md:justify-around justify-between  bg-primary h-16">
+        <div className="flex flex-col drawer-content">
+          <div className="w-full navbar md:justify-around justify-between  bg-primary h-16">
             <div className="flex w-auto items-center md:px-6 px-4">
               <img className="lg:w-10 w-8" src="./logo.svg" alt="logo" />
               <h1 className="lg:text-2xl text-xl font-bold">Libvery</h1>
             </div>
-            <div class="flex-none lg:hidden">
-              <label for="my-drawer-3" class="btn btn-square btn-ghost">
+            <div className="flex-none lg:hidden">
+              <label for="my-drawer-3" className="btn btn-square btn-ghost">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  class="inline-block w-6 h-6 stroke-current"
+                  className="inline-block w-6 h-6 stroke-current"
                 >
                   <path
                     stroke-linecap="round"
@@ -41,27 +47,30 @@ function Navbar() {
                 </svg>
               </label>
             </div>
-            <div class="flex-none hidden lg:block md:justify-around md:flex">
-              <ul class="menu horizontal">
+            <div className="flex-none hidden lg:block md:justify-around md:flex">
+              <ul className="menu horizontal">
                 <li className="border-b-2 mx-4 border-blue-400">
-                  <a href="#" className="text-base text-blue-400 px-4">
+                  <Link
+                    to={HOME_ROUTE}
+                    className="text-base text-blue-400 px-4"
+                  >
                     Home
-                  </a>
+                  </Link>
                 </li>
                 <li className="border-b-2 mx-4 border-white">
-                  <a href="#" className="text-base px-4">
+                  <Link to={BOOKS_ROUTE} className="text-base px-4">
                     Books
-                  </a>
+                  </Link>
                 </li>
                 <li className="border-b-2 mx-4 border-white">
-                  <a href="#" className="text-base px-4">
+                  <Link to={ABOUT_ROUTE} className="text-base px-4">
                     About
-                  </a>
+                  </Link>
                 </li>
                 <li className="border-b-2 mx-4 border-white">
-                  <a href="#" className="text-base px-4">
+                  <Link to={FAVOURITES_ROUTE} className="text-base px-4">
                     Favorites
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -80,31 +89,43 @@ function Navbar() {
             </div>
           </div>
         </div>
-        <div class="drawer-side">
+        <div className="drawer-side">
           <label
             for="my-drawer-3"
-            class="w-1/2 h-screen drawer-overlay"
+            className="w-1/2 h-screen drawer-overlay"
           ></label>
-          <ul class="p-4 overflow-y-auto menu justify-start align-middle w-1/2 h-screen bg-black">
+          <ul className="p-4 overflow-y-auto menu justify-start align-middle w-1/2 h-screen bg-black">
             <li className="border-b-2 m-4 border-white">
-              <a href="#" className="text-base p-4 text-white self-center">
+              <Link
+                to={HOME_ROUTE}
+                className="text-base p-4 text-white self-center"
+              >
                 Home
-              </a>
+              </Link>
             </li>
             <li className="border-b-2 m-4 border-white">
-              <a href="#" className="text-base p-4 text-white self-center">
+              <Link
+                to={BOOKS_ROUTE}
+                className="text-base p-4 text-white self-center"
+              >
                 Books
-              </a>
+              </Link>
             </li>
             <li className="border-b-2 m-4 border-white">
-              <a href="#" className="text-base p-4 text-white self-center">
+              <Link
+                to={ABOUT_ROUTE}
+                className="text-base p-4 text-white self-center"
+              >
                 About
-              </a>
+              </Link>
             </li>
             <li className="border-b-2 m-4 border-white">
-              <a href="#" className="text-base p-4 text-white self-center">
+              <Link
+                to={FAVOURITES_ROUTE}
+                className="text-base p-4 text-white self-center"
+              >
                 Favorites
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
