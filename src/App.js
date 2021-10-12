0f0834c48752272b920f44c5d52ff4cdc009c594
Zahraa <66907264+ZhraaBaharAluloom@users.noreply.github.com>
2021-10-12 07:16:25 -0700

    Authentication pages (#25)
    * Update src/assets/Email.svg
    
    Co-authored-by: RawandDev <82733587+RawandDev@users.noreply.github.com>
    
    * installed tailwind forms
    
    * add tailwind forms and spaces to the config file
    
    * add icons to assets file
    
    * design the sign in component
    
    * design the sign up in component
    
    * display auth components for the test stuff
    
    * remove unused code
    
    * remove extra comment
    
    * change the class to className
    
    * resolve conflicts again
    
    * install formik
    
    * add formik to sign up page
    
    * install localization dependencies
    
    * create a translations files
    
    * add reacti18next configration
    
    * add translation to about page
    
    * add translation to auth pages
    
    * add translation to sign up page
    
    * Revert "add formik to sign up page"
    
    This reverts commit a003122f08010f77542a33f0099131bc4dffeb0f.
    
    * Revert "create a translations files"
    
    This reverts commit 3c736567c3d561d2feaa27d32c5144d1f9a6c20d.
    
    * Revert "add reacti18next configration"
    
    This reverts commit 443828a9760b72e5747a7e73a1cc6883c15aef3e.
    
    * Revert "add translation to about page"
    
    This reverts commit 2cb1d16da375c64d55ebd76ed4772c191eef221b.
    
    * Revert "add translation to auth pages"
    
    This reverts commit ee36e5eae038b38710f36cb7a4a496ef5db168f4.
    
    * Revert "add translation to sign up page"
    
    This reverts commit 7b3781c5cc9f945fb07341f9de8a452238d10dec.
    
    * Add formik to sign up page
    
    * add validationSchema
    
    Co-authored-by: RawandDev <82733587+RawandDev@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { Switch, Route } from "react-router-dom";
 import About from "./pages/About";
 import Books from "./pages/Books";
@@ -6,8 +7,8 @@ import Error from "./pages/Error";
 import Favourites from "./pages/Favourites";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import Signin from "./pages/SignIn";
-import Signup from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import Navbar from "./components/Navbar.jsx";
 import {
   HOME_ROUTE,
@@ -15,6 +16,8 @@ import {
   BOOKS_ROUTE,
   FAVOURITES_ROUTE,
   SIGNIN_ROUTE,
+  SIGNUP_ROUTE,
+  ERROR_ROUTE,
   PROFILE_ROUTE,
 } from "./routes";
 import Footer from "./components/Footer";
@@ -29,8 +32,9 @@ function App() {
         <Route path={ABOUT_ROUTE} component={About} />
         <Route path={BOOKS_ROUTE} component={Books} />
         <Route path={FAVOURITES_ROUTE} component={Favourites} />
-        <Route path={SIGNIN_ROUTE} component={Signup} />
-        <Route path={SIGNIN_ROUTE} component={Signin} />
+        <Route path={SIGNUP_ROUTE} component={SignUp} />
+        <Route path={SIGNIN_ROUTE} component={SignIn} />
+        <Route path={ERROR_ROUTE} component={Error} />
         <Route path={PROFILE_ROUTE} component={Profile} />
         <Route component={Error} />
       </Switch>
