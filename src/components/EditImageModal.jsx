a975fe9c2006ee4e707f942204b23f8585800fda
RawandDev <rawand.200091384037@gmail.com>
2021-10-13 01:09:31 +0300

    refactor(icon): refactor icons

--- src/components/EditImageModal.jsx
+++ src/components/EditImageModal.jsx
@@ -3,6 +3,9 @@ import Modal from "react-modal";
 import CloseButton from "../helpers/CloseButton";
 import { motion } from "framer-motion";
 import { useHistory } from "react-router-dom";
+import editIcon from "../assets/edit.svg";
+import trashIcon from "../assets/trash.svg";
+import plusIcon from "../assets/plus.svg";
 
 const el = document.getElementById("root");
 Modal.setAppElement(el);
@@ -57,38 +60,38 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen }) {
             </h2>
             <div className="flex items-center">
               <label className="w-32 text-white flex items-center py-1 rounded-md cursor-pointer bg-secondary bg-opacity-50 hover:bg-secondary justify-center h-32 mx-auto mb-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  {" "}
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 4v16m8-8H4"
-                  />{" "}
-                </svg>
+                <img src={plusIcon} alt="plus icon" className="w-10 h-10" />
                 <input type="file" className="hidden" />
               </label>
               <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 bg-black rounded-full mb-4 cursor-pointer"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                  />
-                </svg>
+                <img
+                  src={editIcon}
+                  alt=""
+                  className="bg-black w-6 mb-4 rounded-full cursor-pointer"
+                />
+                <img
+                  src={trashIcon}
+                  alt=""
+                  className="bg-black rounded-full cursor-pointer"
+                />
+              </div>
+            </div>
+            <div className="flex items-center">
+              <label className="w-32 text-white flex items-center py-1 rounded-md cursor-pointer bg-secondary bg-opacity-50 hover:bg-secondary justify-center h-32 mx-auto mb-2">
+                <img src={plusIcon} alt="plus icon" className="w-10 h-10" />
+                <input type="file" className="hidden" />
+              </label>
+              <div>
+                <img
+                  src={editIcon}
+                  alt=""
+                  className="bg-black w-6 mb-4 rounded-full cursor-pointer"
+                />
+                <img
+                  src={trashIcon}
+                  alt=""
+                  className="bg-black rounded-full cursor-pointer"
+                />
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-6 w-6 bg-black rounded-full cursor-pointer"
@@ -107,102 +110,20 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen }) {
             </div>
             <div className="flex items-center">
               <label className="w-32 text-white flex items-center py-1 rounded-md cursor-pointer bg-secondary bg-opacity-50 hover:bg-secondary justify-center h-32 mx-auto mb-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  {" "}
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 4v16m8-8H4"
-                  />{" "}
-                </svg>
+                <img src={plusIcon} alt="plus icon" className="w-10 h-10" />
                 <input type="file" className="hidden" />
               </label>
               <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 bg-black rounded-full mb-4 cursor-pointer"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                  />
-                </svg>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 bg-black rounded-full cursor-pointer"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                  />
-                </svg>
-              </div>
-            </div>
-            <div className="flex items-center">
-              <label className="w-32 text-white flex items-center py-1 rounded-md cursor-pointer bg-secondary bg-opacity-50 hover:bg-secondary justify-center h-32 mx-auto mb-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  {" "}
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 4v16m8-8H4"
-                  />{" "}
-                </svg>
-                <input type="file" className="hidden" />
-              </label>
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 bg-black rounded-full mb-4 cursor-pointer"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                  />
-                </svg>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 bg-black rounded-full cursor-pointer"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                  />
-                </svg>
+                <img
+                  src={editIcon}
+                  alt=""
+                  className="bg-black w-6 mb-4 rounded-full cursor-pointer"
+                />
+                <img
+                  src={trashIcon}
+                  alt=""
+                  className="bg-black rounded-full cursor-pointer"
+                />
               </div>
             </div>
             <button
