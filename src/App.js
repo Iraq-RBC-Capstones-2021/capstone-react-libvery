b42a9fa3b54eb26fd7bba46eba461473f4567cbf
Zahraa <alwarda@hotmail.co.uk>
2021-10-13 00:20:06 -0700

    fix the routes , remove extra code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-
 import { Switch, Route } from "react-router-dom";
+
 import About from "./pages/About";
 import Books from "./pages/Books";
 import Error from "./pages/Error";
@@ -9,7 +9,9 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar.jsx";
+
 import {
   HOME_ROUTE,
   ABOUT_ROUTE,
@@ -20,15 +22,12 @@ import {
   ERROR_ROUTE,
   PROFILE_ROUTE,
 } from "./routes";
-import Footer from "./components/Footer";
-import BookCard from "./components/BookCard";
 
 function App() {
   return (
     <>
       <Navbar />
       <Switch>
-        <Route exact path={HOME_ROUTE} component={Home} />
         <Route path={ABOUT_ROUTE} component={About} />
         <Route path={BOOKS_ROUTE} component={Books} />
         <Route path={FAVOURITES_ROUTE} component={Favourites} />
@@ -36,16 +35,9 @@ function App() {
         <Route path={SIGNIN_ROUTE} component={SignIn} />
         <Route path={ERROR_ROUTE} component={Error} />
         <Route path={PROFILE_ROUTE} component={Profile} />
+        <Route exact path={HOME_ROUTE} component={Home} />
         <Route component={Error} />
       </Switch>
-      {/* to showcase the BookCard, Will be removed before merge */}
-      <BookCard
-        image="https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Ym9va3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-        genres={["Action", "Drama", "Romance", "Novel", "Thriller"]}
-        title="Book Title"
-        rating="2.5(5)"
-        price="9.99$"
-      />
       <Footer />
     </>
   );
