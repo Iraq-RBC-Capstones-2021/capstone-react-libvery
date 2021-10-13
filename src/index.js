84e6ed8ad75f42c101749a56e62575cdf0695c7b
Zahraa <alwarda@hotmail.co.uk>
2021-10-13 00:20:25 -0700

    add i18next configration

--- src/index.js
+++ src/index.js
@@ -7,6 +7,26 @@ import { store } from "./store";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ["en", "ar", "kurd"],
+    fallbackLng: "en",
+    detection: {
+      order: ["cookie", "htmlTag", "localStorage", "path", "subdomain"],
+      caches: ["cookie"],
+    },
+    backend: { loadPath: "assets/locales/{{lng}}/translation.json" },
+    react: { useSuspense: false },
+  });
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
