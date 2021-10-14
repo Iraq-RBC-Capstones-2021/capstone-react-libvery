deeb6067169a4033188de28989841c9a7633d25d
RawandDev <82733587+RawandDev@users.noreply.github.com>
2021-10-14 17:56:31 +0300

    [issue34] update about (#35)
    * fix(issue): fix the gap with reversed cards
    
    * feat(social): add social links

--- src/App.js
+++ src/App.js
@@ -21,11 +21,10 @@ import {
   PROFILE_ROUTE,
 } from "./routes";
 import Footer from "./components/Footer";
-import BookCard from "./components/BookCard";
 
 function App() {
   return (
-    <>
+    <div className="bg-primary">
       <Navbar />
       <Switch>
         <Route exact path={HOME_ROUTE} component={Home} />
@@ -38,16 +37,8 @@ function App() {
         <Route path={PROFILE_ROUTE} component={Profile} />
         <Route component={Error} />
       </Switch>
-      {/* to showcase the BookCard, Will be removed before merge */}
-      <BookCard
-        image="https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Ym9va3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-        genres={["Action", "Drama", "Romance", "Novel", "Thriller"]}
-        title="Book Title"
-        rating="2.5(5)"
-        price="9.99$"
-      />
       <Footer />
-    </>
+    </div>
   );
 }
 
