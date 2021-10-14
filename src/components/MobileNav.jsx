008fc5189b95d4e60f796779c6c0c51f7c5e4998
RawandDev <rawand.200091384037@gmail.com>
2021-10-15 00:50:02 +0300

    feat(navbar): add navbar option

---
+++ src/components/MobileNav.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import {
+  HOME_ROUTE,
+  ABOUT_ROUTE,
+  PROFILE_ROUTE,
+  FAVOURITES_ROUTE,
+  SIGNIN_ROUTE,
+  SIGNUP_ROUTE,
+  BOOKS_ROUTE,
+} from "../routes";
+import userIcon from "../assets/userPlaceholder.svg";
+
+function MobileNav({ navbarOpen }) {
+  const [isOptionOpened, setIsOptionOpened] = useState(false);
+
+  const styles = {
+    borderBottom: "2px solid #55b8ef",
+    width: "15%",
+    color: "#55b8ef",
+  };
+
+  const location = useLocation();
+
+  const isActive = (route) => {
+    return location.pathname === route ? "active" : "";
+  };
+
+  console.log(isActive(HOME_ROUTE));
+
+  return (
+    <>
+      <div className={navbarOpen ? "block" : "hidden"}>
+        <div className="sm:hidden flex flex-col justify-center text-black uppercase mx-5">
+          <NavLink
+            to={HOME_ROUTE}
+            activeStyle={isActive(HOME_ROUTE) ? styles : ""}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to={BOOKS_ROUTE}
+            activeStyle={isActive(BOOKS_ROUTE) ? styles : ""}
+          >
+            Books
+          </NavLink>
+          <NavLink
+            to={ABOUT_ROUTE}
+            activeStyle={isActive(ABOUT_ROUTE) ? styles : ""}
+          >
+            About
+          </NavLink>
+          <NavLink
+            to={FAVOURITES_ROUTE}
+            activeStyle={isActive(FAVOURITES_ROUTE) ? styles : ""}
+          >
+            Favourites
+          </NavLink>
+          {/* <Link to={PROFILE_ROUTE}> */}
+          <div className="flex items-end">
+            <img
+              src={userIcon}
+              alt="user"
+              className="w-10 h-10 border-2 rounded-full"
+            />
+            <p className="opacity-50">Username</p>
+            <div class="relative inline-block text-left">
+              <div>
+                <button
+                  onClick={() => setIsOptionOpened(!isOptionOpened)}
+                  type="button"
+                  class="font-semibold relative"
+                  id="menu-button"
+                  aria-expanded="true"
+                  aria-haspopup="true"
+                >
+                  <svg
+                    class="w-8 absolute -bottom-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
+              {isOptionOpened ? (
+                <div
+                  class="origin-top-right absolute w-44 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="menu-button"
+                  tabindex="-1"
+                >
+                  <div class="py-1" role="none">
+                    <Link
+                      to={PROFILE_ROUTE}
+                      class="text-gray-700 block px-4 py-2 text-sm"
+                      role="menuitem"
+                      tabindex="-1"
+                      id="menu-item-0"
+                    >
+                      Profile
+                    </Link>
+                    <form method="POST" action="#" role="none">
+                      <button
+                        type="submit"
+                        class="text-gray-700 block w-full text-left px-4 py-2 text-sm"
+                        role="menuitem"
+                        tabindex="-1"
+                        id="menu-item-3"
+                      >
+                        Sign out
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              ) : (
+                <div></div>
+              )}
+            </div>
+          </div>
+          {/* </Link> */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MobileNav;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import Menu from "./Menu";
+import MobileNav from "./MobileNav";
 import { Link } from "react-router-dom";
 import {
-  ABOUT_ROUTE,
-  BOOKS_ROUTE,
-  FAVOURITES_ROUTE,
   HOME_ROUTE,
+  ABOUT_ROUTE,
+  PROFILE_ROUTE,
+  FAVOURITES_ROUTE,
+  SIGNIN_ROUTE,
+  SIGNUP_ROUTE,
+  BOOKS_ROUTE,
 } from "../routes";
+import logoIcon from "../assets/logo.svg";
 
 function Navbar() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  function handleShowMenu() {
+    setNavbarOpen((prev) => !prev);
+  }
+
   return (
-    <div>
-      <Link to={HOME_ROUTE}>Home</Link>
-      <Link to={ABOUT_ROUTE}>About</Link>
-      <Link to={FAVOURITES_ROUTE}>Favourites</Link>
-      <Link to={BOOKS_ROUTE}>Books</Link>
+    <div className={navbarOpen ? "bg-white" : "bg-primary"}>
+      <div className="flex justify-between mx-4">
+        <div>
+          <img src={logoIcon} alt="logo" className="w-32" />
+        </div>
+        <div className="">
+          <div className="hidden sm:flex">
+            <Link to={HOME_ROUTE}>Home</Link>
+            <Link to={BOOKS_ROUTE}>Books</Link>
+            <Link to={ABOUT_ROUTE}>About</Link>
+            <Link to={FAVOURITES_ROUTE}>Favourites</Link>
+          </div>
+        </div>
+        <Menu navbarOpen={navbarOpen} onShowMenu={handleShowMenu} />
+      </div>
+      <MobileNav navbarOpen={navbarOpen} onShowMenu={handleShowMenu} />
     </div>
   );
 }
