40596d43e9e417cf3c4faacda8c5cf9a42ce7e74
RawandDev <rawand.200091384037@gmail.com>
2021-10-14 23:05:39 +0300

    feat(routing): add condition to match the routing

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 function Profile(props) {
   const match = props.match.url;
+  const location = useLocation();
+
+  console.log(location.pathname);
 
   console.log(match);
 
@@ -51,48 +54,53 @@ function Profile(props) {
           User Info
         </NavLink>
       </div>
-
-      <div className=" w-full max-w-sm md:max-w-sm m-auto">
-        <form className="rounded px-0 pt-6 pb-8 ">
-          <div className="mb-4">
-            <label className="block text-gray-500 text-sm mb-2">Username</label>
-            <input
-              className=" shadow appearance-none border-none rounded-lg  focus:outline-none outline-none w-full py-3 px-3 text-gray-500 font leading-tight  focus:shadow-outline font-extralight text-sm"
-              id="username"
-              type="text"
-              placeholder="EX: Jack"
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-500 font text-sm mb-2">
-              Phone Number
-            </label>
-            <input
-              className="  border-none w-full 	 shadow appearance-none border  rounded-lg  py-3 px-3 text-gray-500 font mb-3 leading-tight focus:outline-none focus:shadow-outline font-extralight text-sm"
-              id="password"
-              type="password"
-              placeholder="EX: JKSmdno&297760"
-            />
-          </div>
-          <div className="flex items-center justify-center m-2 ">
-            <button
-              className="bg-black hover:bg-blue-700 text-white py-2 px-4 w-44 rounded-lg focus:outline-none focus:shadow-outline font-light"
-              type="button"
-            >
-              Edit
-              {/* TODO: when the user presses on edit button the text should change to "Save Changes" */}
-            </button>
-          </div>
-          <div className="flex items-center justify-center ">
-            <button
-              className=" bg-red-800  hover:bg-blue-700 text-white py-2 px-4 w-44 rounded-lg focus:outline-none focus:shadow-outline font-light"
-              type="button"
-            >
-              Log Out
-            </button>
-          </div>
-        </form>
-      </div>
+      {location.pathname === "/profile/user" ? (
+        <div className=" w-full max-w-sm md:max-w-sm m-auto">
+          <form className="rounded px-0 pt-6 pb-8 ">
+            <div className="mb-4">
+              <label className="block text-gray-500 text-sm mb-2">
+                Username
+              </label>
+              <input
+                className=" shadow appearance-none border-none rounded-lg  focus:outline-none outline-none w-full py-3 px-3 text-gray-500 font leading-tight  focus:shadow-outline font-extralight text-sm"
+                id="username"
+                type="text"
+                placeholder="EX: Jack"
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-500 font text-sm mb-2">
+                Phone Number
+              </label>
+              <input
+                className="  border-none w-full 	 shadow appearance-none border  rounded-lg  py-3 px-3 text-gray-500 font mb-3 leading-tight focus:outline-none focus:shadow-outline font-extralight text-sm"
+                id="password"
+                type="password"
+                placeholder="EX: JKSmdno&297760"
+              />
+            </div>
+            <div className="flex items-center justify-center m-2 ">
+              <button
+                className="bg-black hover:bg-blue-700 text-white py-2 px-4 w-44 rounded-lg focus:outline-none focus:shadow-outline font-light"
+                type="button"
+              >
+                Edit
+                {/* TODO: when the user presses on edit button the text should change to "Save Changes" */}
+              </button>
+            </div>
+            <div className="flex items-center justify-center ">
+              <button
+                className=" bg-red-800  hover:bg-blue-700 text-white py-2 px-4 w-44 rounded-lg focus:outline-none focus:shadow-outline font-light"
+                type="button"
+              >
+                Log Out
+              </button>
+            </div>
+          </form>
+        </div>
+      ) : (
+        <div>Maybe Favourites or List of Books :D</div>
+      )}
     </div>
   );
 }
