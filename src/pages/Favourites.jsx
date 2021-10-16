be20b5952f6c22dfb6d932fa47115a905d658553
Niyaz Irfan <niyaz.irfan119@gmail.com>
2021-10-17 00:34:21 +0300

    create favorite page

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,9 +1,94 @@
 import React from "react";
+import BookCard from "../components/BookCard";
 
 function Favourites() {
+  const books = [
+    {
+      id: 1,
+      title: "The Lord of the Rings",
+      genres: ["Fantasy", "Novel", "Drama", "Adventure", "Action"],
+      image:
+        "https://images.unsplash.com/photo-1532012197267-da84d127e765?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",
+      rating: 5,
+      price: "10.99$",
+    },
+    {
+      id: 2,
+      title: "The Hobbit",
+      genres: ["Fantasy", "Adventure"],
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX331_BO1,204,203,200_.jpg",
+      rating: 5,
+      price: "10.99$",
+    },
+    {
+      id: 3,
+      title: "The Lord of the Rings",
+      genres: ["Fantasy", "Adventure", "Drama"],
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX331_BO1,204,203,200_.jpg",
+      rating: 5,
+      price: "10.99$",
+    },
+    {
+      id: 4,
+      title: "The Hobbit",
+      genres: ["Fantasy", "Adventure"],
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX331_BO1,204,203,200_.jpg",
+      rating: 5,
+      price: "10.99$",
+    },
+    {
+      id: 5,
+      title: "The Lord of the Rings",
+      genres: ["Fantasy", "Adventure", "Drama"],
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX331_BO1,204,203,200_.jpg",
+      rating: 5,
+      price: "10.99$",
+    },
+    {
+      id: 6,
+      title: "The Hobbit",
+      genres: ["Fantasy", "Adventure"],
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX331_BO1,204,203,200_.jpg",
+      rating: 5,
+      price: "10.99$",
+    },
+
+    {
+      id: 7,
+      title: "The Lord of the Rings",
+      genres: ["Fantasy", "Adventure", "Drama"],
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX331_BO1,204,203,200_.jpg",
+      rating: 5,
+      price: "10.99$",
+    },
+  ];
+
+  const booksArr = books.map((book) => (
+    <div key={book.id} className="m-2">
+      <BookCard
+        id={book.id}
+        title={book.title}
+        genres={book.genres}
+        image={book.image}
+        rating={book.rating}
+        price={book.price}
+      />
+    </div>
+  ));
   return (
-    <div>
-      <h1>Favourites</h1>
+    <div className="flex flex-col ">
+      <h1 className="p-3 mt-3 shadow appearance-none font-sans font-semibold text-gray-800 self-center tracking-wide text-lg sm:text-xl md:text-2xl lg:text-3xl">
+        Your Favorite Books Are Here
+      </h1>
+      <div className="grid sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:justify-center py-5">
+        {booksArr}
+      </div>
     </div>
   );
 }
