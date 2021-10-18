83472929ad35a6e0c0991130746eed43ca78d35b
Niyaz Irfan <niyaz.irfan119@gmail.com>
2021-10-18 16:57:37 +0300

    create pagination component and css

---
+++ src/components/pagination/Pagination.jsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import "./pagination.css";
+
+function Pagination({ pages = 10 }) {
+  const numberOfPages = [];
+
+  for (let i = 1; i <= pages; i++) {
+    numberOfPages.push(i);
+  }
+
+  const [currentButton, setCurrentButton] = useState(1);
+  //state for what buttons are displayed currently
+  const [arrOfCurrButtons, setArrOfCurrButtons] = useState([]);
+
+  useEffect(() => {
+    let tempNumberOfPages = [...numberOfPages];
+
+    let dotsInitial = "...";
+    let dotsLeft = "... ";
+    let dotsRight = " ...";
+
+    if (numberOfPages.length < 6) {
+      tempNumberOfPages = numberOfPages;
+    } else if (currentButton >= 1 && currentButton <= 3) {
+      tempNumberOfPages = [1, 2, 3, 4, dotsInitial, numberOfPages.length];
+    } else if (currentButton === 4) {
+      const sliced = numberOfPages.slice(0, 5);
+      tempNumberOfPages = [...sliced, dotsInitial, numberOfPages.length];
+    } else if (currentButton > 4 && currentButton < numberOfPages.length - 2) {
+      // from 5 to 8 -> (10 - 2)
+      const sliced1 = numberOfPages.slice(currentButton - 2, currentButton); // sliced1 (5-2, 5) -> [4,5]
+      const sliced2 = numberOfPages.slice(currentButton, currentButton + 1); // sliced1 (5, 5+1) -> [6]
+      tempNumberOfPages = [
+        1,
+        dotsLeft,
+        ...sliced1,
+        ...sliced2,
+        dotsRight,
+        numberOfPages.length,
+      ]; // [1, '...', 4, 5, 6, '...', 10]
+    } else if (currentButton > numberOfPages.length - 3) {
+      // > 7
+      const sliced = numberOfPages.slice(numberOfPages.length - 4); // slice(10-4)
+      tempNumberOfPages = [1, dotsLeft, ...sliced];
+    } else if (currentButton === dotsInitial) {
+      // [1, 2, 3, 4, "...", 10].length = 6 - 3  = 3
+      // arrOfCurrButtons[3] = 4 + 1 = 5
+      // or
+      // [1, 2, 3, 4, 5, "...", 10].length = 7 - 3 = 4
+      // [1, 2, 3, 4, 5, "...", 10][4] = 5 + 1 = 6
+      setCurrentButton(arrOfCurrButtons[arrOfCurrButtons.length - 3] + 1);
+    } else if (currentButton === dotsRight) {
+      setCurrentButton(arrOfCurrButtons[3] + 2);
+    } else if (currentButton === dotsLeft) {
+      setCurrentButton(arrOfCurrButtons[3] - 2);
+    }
+
+    setArrOfCurrButtons(tempNumberOfPages);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentButton]);
+
+  return (
+    <div>
+      <div className="pagination-container mb-5">
+        <span
+          className={currentButton === 1 ? "disabled" : ""}
+          onClick={() =>
+            setCurrentButton((prev) => (prev === 1 ? prev : prev - 1))
+          }
+        >
+          Prev
+        </span>
+        {arrOfCurrButtons.map((page, index) => {
+          return (
+            <span
+              onClick={() => setCurrentButton(page)}
+              key={index}
+              className={currentButton === page ? "active" : ""}
+            >
+              {page}
+            </span>
+          );
+        })}
+        <span
+          className={currentButton === numberOfPages.length ? "disabled" : ""}
+          onClick={() =>
+            setCurrentButton((prev) =>
+              prev === numberOfPages.length ? prev : prev + 1
+            )
+          }
+        >
+          Next
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Pagination;
