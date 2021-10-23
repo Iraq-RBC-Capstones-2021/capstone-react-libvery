4327d42ba56e983217a417f663010aa4bf19e897
Zahraa <alwarda@hotmail.co.uk>
2021-10-22 17:28:09 -0700

    create a signout component

---
+++ src/components/SignoutButton.jsx
@@ -0,0 +1,28 @@
+import { signOut } from "@firebase/auth";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { auth } from "../firebase.js";
+import { setLogOut } from "../store/counter/userSlice.js";
+import { useHistory } from "react-router";
+
+const SignoutButton = ({ className, setIsOptionOpened }) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        dispatch(setLogOut());
+        history.replace("/");
+        setIsOptionOpened(false);
+      })
+      .catch((error) => console.log(error.message));
+  };
+  return (
+    <button className={className} type="button" onClick={handleSignOut}>
+      Log Out
+    </button>
+  );
+};
+
+export default SignoutButton;
