055e9e5b39029e9c67738642cd1b66e243210909
Zahraa <alwarda@hotmail.co.uk>
2021-10-22 17:27:50 -0700

    get the user info from store and display it in userInfo

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  selectorUserName,
+  selectorUserEmail,
+  selectorUserPhone,
+  selectorUserPhoto,
+} from "../store/counter/userSlice.js";
 
 import UserInfo from "./UserInfo";
 import Favourites from "./Favourites";
@@ -7,21 +14,31 @@ import Books from "./Books";
 
 function Profile(props) {
   const match = props.match.url;
+
   const location = useLocation();
 
-  // TODO: get the user info from the store and pass it to the UserInfo component
+  const userName = useSelector(selectorUserName);
+  const userEmail = useSelector(selectorUserEmail);
+  const userPhone = useSelector(selectorUserPhone);
+  const userPhoto = useSelector(selectorUserPhoto);
   // TODO: get the books list from the store, filter them to get the books that belong to this user only, and pass the list to the books component to display them
   // TODO: get the books from the store and filter them to get the books that their "isBookmarked" === true and pass it to the favorites component to display them
 
   return (
     <div className="min-h-screen space-y-5 p-10 bg-primary font-sans ">
       <img
-        src="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
+        src={
+          userPhoto
+            ? userPhoto
+            : `https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg`
+        }
         alt=""
         className="rounded-full m-auto"
         width="200em"
       />
-      <p className="text-center text-xl  font-sans text-gray-500 ">@Username</p>
+      <p className="text-center text-xl  font-sans text-gray-500 ">
+        @{userName}
+      </p>
       <div className="flex justify-between bg-white rounded-xl items-center sm:max-w-sm mx-auto">
         <NavLink
           to={`${match}/favourites`}
@@ -67,7 +84,11 @@ function Profile(props) {
       </div>
 
       {location.pathname === "/profile/user" ? (
-        <UserInfo />
+        <UserInfo
+          userName={userName}
+          userEmail={userEmail}
+          userPhone={userPhone}
+        />
       ) : location.pathname === "/profile/favourites" ? (
         <Favourites />
       ) : (
