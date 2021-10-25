fcb417a461e4d4f259daac751562740beff44675
Zahraa <66907264+ZhraaBaharAluloom@users.noreply.github.com>
2021-10-25 01:50:28 -0700

    Connect auth pages to firebase  (#59)
    * create an env file and add it to gitigonre
    
    * add google authentication, connect it to redux
    
    * connect custom auth pages to firebase and save it in redux
    
    * add some permissions conditions for the user in navbar
    
    * get the user info from store and display it in userInfo
    
    * create a signout component
    
    * create userSlice
    
    * some extra stuff
    
    * add uid to the user object and save it in store
    
    * remove unused console.log
    
    * add uid to storage selector selector
    
    * add onAuthStateChed to check the current user
    
    * move the menu icon to a separate component
    
    * remove extra comment
    
    * move the upload icon to its component and add loder
    
    * remove local storage
    
    * fix loading condition
    
    * fix a bug

--- src/pages/UserInfo.jsx
+++ src/pages/UserInfo.jsx
@@ -1,46 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import SignoutButton from "../components/SignoutButton";
 
-const UserInfo = () => {
-  //TODO: get the user info as a prop from its parent (Profile page)
+const UserInfo = ({ userName, userEmail, userPhone }) => {
+  const [showEditForm, setShowEditForm] = useState(false);
+  const handleShowForm = () => {
+    setShowEditForm(!showEditForm);
+  };
   return (
     <div className=" w-full max-w-sm md:max-w-sm m-auto">
       <form className="rounded px-0 pt-6 pb-8 ">
-        <div className="mb-4">
-          <label className="block text-gray-500 text-sm mb-2">Username</label>
-          <input
-            className=" shadow appearance-none border-none rounded-lg  focus:outline-none outline-none w-full py-3 px-3 text-gray-500 font leading-tight  focus:shadow-outline font-extralight text-sm"
-            id="username"
-            type="text"
-            placeholder="EX: Jack"
-          />
-        </div>
-        <div className="mb-6">
-          <label className="block text-gray-500 font text-sm mb-2">
-            Phone Number
-          </label>
-          <input
-            className="  border-none w-full 	 shadow appearance-none border  rounded-lg  py-3 px-3 text-gray-500 font mb-3 leading-tight focus:outline-none focus:shadow-outline font-extralight text-sm"
-            id="password"
-            type="password"
-            placeholder="EX: JKSmdno&297760"
-          />
-        </div>
+        {showEditForm ? (
+          <>
+            <div className="mb-4">
+              <label className="block text-gray-500 text-sm mb-2">
+                Username
+              </label>
+              <input
+                className=" shadow appearance-none border-none rounded-lg  focus:outline-none outline-none w-full py-3 px-3 text-gray-500 font leading-tight  focus:shadow-outline font-extralight text-sm"
+                id="username"
+                type="text"
+                placeholder="EX: Jack"
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-500 font text-sm mb-2">
+                Phone Number
+              </label>
+              <input
+                className="border-none w-full 	 shadow appearance-none border  rounded-lg  py-3 px-3 text-gray-500 font mb-3 leading-tight focus:outline-none focus:shadow-outline font-extralight text-sm"
+                id="tel"
+                type="tel"
+                placeholder="EX: 078012345678"
+              />
+            </div>
+          </>
+        ) : (
+          <div className="space-y-3 mb-10 font-bold text-lg">
+            <p>Name: {userName} </p>
+            <p>Email: {userEmail} </p>
+            <p>
+              Phone Number:{" "}
+              {userPhone ? userPhone : `"🎃 👇 Add a phone number"`}{" "}
+            </p>
+          </div>
+        )}
+
         <div className="flex items-center justify-center m-2 ">
           <button
             className="bg-black hover:bg-blue-700 text-white py-2 px-4 w-44 rounded-lg focus:outline-none focus:shadow-outline font-light"
             type="button"
+            onClick={handleShowForm}
           >
-            Edit
-            {/* TODO: when the user presses on edit button the text should change to "Save Changes" */}
+            {showEditForm ? "Save Changes" : "Edit"}
           </button>
         </div>
         <div className="flex items-center justify-center ">
-          <button
-            className=" bg-red-800  hover:bg-blue-700 text-white py-2 px-4 w-44 rounded-lg focus:outline-none focus:shadow-outline font-light"
-            type="button"
-          >
-            Log Out
-          </button>
+          <SignoutButton className=" bg-red-800  hover:bg-blue-700 text-white py-2 px-4 w-44 rounded-lg focus:outline-none focus:shadow-outline font-light" />
         </div>
       </form>
     </div>
