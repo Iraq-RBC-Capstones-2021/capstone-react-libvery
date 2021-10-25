ea684e7ba489b09e4af11dd55938a96fa214a69d
Niyaz Irfan <niyaz.irfan119@gmail.com>
2021-10-26 00:24:29 +0300

    add functionality for getting back 3 latest books and fixed wave

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Slider from "../components/Slider";
 import homeIllustration from "../assets/homeIllustration.svg";
 
 import { BOOKS_ROUTE } from "../routes";
 import { motion } from "framer-motion";
-
-let slides = [
-  <img
-    src="https://static.vecteezy.com/system/resources/thumbnails/000/217/193/small_2x/motivational-book-cover-with-hand-lettering.jpg"
-    alt="1"
-  />,
-  <img
-    src="https://static.vecteezy.com/system/resources/thumbnails/002/110/581/small_2x/abstract-geometric-cover-design-free-vector.jpg"
-    alt="2"
-  />,
-  <img
-    src="https://target.scene7.com/is/image/Target/GUEST_f0a2a9ed-bfab-45c3-abd2-083d2dc5eeb9"
-    alt="3"
-  />,
-  <img
-    src="https://static.vecteezy.com/system/resources/thumbnails/000/216/149/small_2x/MOTIVATIONAL_BOOK_COVER_MAY_18_EEZY_01.jpg"
-    alt="4"
-  />,
-  <img
-    src="https://i.pinimg.com/originals/47/e2/ac/47e2ac4acd0fbda6f28cf2fe67f60835.jpg"
-    alt="5"
-  />,
-];
+import { db } from "../firebase";
+import {
+  query,
+  orderBy,
+  onSnapshot,
+  limit,
+  collection,
+} from "firebase/firestore";
 
 const Home = () => {
+  const [slides, setSlides] = useState([]);
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "books"),
+      orderBy("createdAt", "desc"),
+      limit(3)
+    );
+    const unsub = onSnapshot(q, (snapshot) =>
+      setSlides(snapshot.docs.map((doc) => doc.data().image))
+    );
+
+    return unsub;
+  }, []);
+
+  const slidesArr = slides.map((slide, index) => {
+    return (
+      <img
+        className="w-64 h-72 sm:w-72 sm:h-80 md:w-80 md:h-84 lg:w-84 lg:h-96"
+        src={slide}
+        alt={index + 1}
+      />
+    );
+  });
+
   return (
     <div className="overflow-y-hidden">
       <div className="min-h-screen bg-primary font-sans overflow-x-hidden">
@@ -56,10 +66,10 @@ const Home = () => {
             </Link>
           </div>
           <div className="rounded-xl md:w-12/12 lg:w-7/12 mt-20 md:mb-28 overflow-hidden">
-            <Slider slides={slides} />
+            <Slider slides={slidesArr} />
           </div>
         </motion.div>
-        <div className="absolute bottom-2 transform translate-y-12 w-full left-0">
+        <div className="absolute bottom-0 -m-8 transform translate-y-12 w-full left-0">
           <img src={homeIllustration} alt="" width="100%" />
         </div>
       </div>
