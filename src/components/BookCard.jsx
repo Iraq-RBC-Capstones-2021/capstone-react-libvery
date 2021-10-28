b953a4b9cfb14f007f3c6a72d581d7564d5562ca
Zahraa <alwarda@hotmail.co.uk>
2021-10-28 11:03:08 -0700

    add handler function handles heart icon button

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactStars from "react-rating-stars-component";
 import { Link } from "react-router-dom";
 import { BOOKS_ROUTE } from "../routes";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
-
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import {
+  selectorUser,
+  setFavorites,
+  setRemoveFavorites,
+} from "../store/counter/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 function BookCard({ image, genres, title, price, rating, id }) {
+  const user = useSelector(selectorUser);
+
   const genreButtons = genres.map((genre, index) => {
     return (
       <button
@@ -16,9 +25,25 @@ function BookCard({ image, genres, title, price, rating, id }) {
     );
   });
 
-  // we will get this from firestore as boolean
-  const [isFavorite, setIsFavorite] = useState(false);
-
+  const dispatch = useDispatch();
+  const handleFav = async (id) => {
+    if (!user.favorites.map((book) => book.book.id).includes(Number(id))) {
+      const docRef = doc(db, "books", `${id}`);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        dispatch(setFavorites({ book: docSnap.data() }));
+      } else {
+        console.log("No such document!");
+      }
+    } else {
+      const docRef = doc(db, "books", `${id}`);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        dispatch(setRemoveFavorites(docSnap.data().id));
+      }
+    }
+  };
   return (
     <div className="bg-cards rounded-xl font-sans  overflow-hidden shadow-md w-full md:w-60 lg:w-72  p-3 transform transition ease-in-out duration-200 hover:-translate-y-0.5 mx-auto md:mt-5 mb-1 sm:mb-2 md:mb-0">
       <div className="rounded-xl overflow-hidden relative h-60">
@@ -29,16 +54,16 @@ function BookCard({ image, genres, title, price, rating, id }) {
       </div>
       <div className="flex justify-between items-center px-1 pt-2">
         <h1 className="font-semibold">{title}</h1>
-        {isFavorite ? (
+        {user.favorites.map((book) => book.book.id).includes(Number(id)) ? (
           <AiFillHeart
-            onClick={() => setIsFavorite((prev) => !prev)}
+            onClick={() => handleFav(id)}
             size={29}
             className="transform transition ease-in duration-100 hover:-translate-y-0.5 cursor-pointer"
             color={"red"}
           />
         ) : (
           <AiOutlineHeart
-            onClick={() => setIsFavorite((prev) => !prev)}
+            onClick={() => handleFav(id)}
             size={29}
             className="transform transition ease-in duration-100 hover:-translate-y-0.5 cursor-pointer"
           />
