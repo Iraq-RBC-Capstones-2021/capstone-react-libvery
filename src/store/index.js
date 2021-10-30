39bb1849bcc989c2ac9abb081c33976ade3481e1
Niyaz Irfan <niyaz.irfan119@gmail.com>
2021-10-30 12:43:48 +0300

    adding search and click on genres functionality

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksSlice from "./booksSlice";
 import userSlice from "./counter/userSlice";
+import dropdownSlice from "./dropdownSlice";
 
 export const store = configureStore({
   reducer: {
     books: booksSlice,
     user: userSlice,
+    dropdown: dropdownSlice,
   },
 });
