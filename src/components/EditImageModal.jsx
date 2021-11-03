b03de6cbc67a0d68c97bb13696c15eb7bf25623a
Zahraa <alwarda@hotmail.co.uk>
2021-11-03 13:17:50 -0700

    fix update images function

--- src/components/EditImageModal.jsx
+++ src/components/EditImageModal.jsx
@@ -6,7 +6,7 @@ import { useHistory } from "react-router-dom";
 import editIcon from "../assets/edit.svg";
 import trashIcon from "../assets/trash.svg";
 import plusIcon from "../assets/plus.svg";
-import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+import { doc, updateDoc, arrayRemove } from "firebase/firestore";
 import { db } from "../firebase";
 import { getDownloadURL, ref, uploadBytes } from "@firebase/storage";
 import { storage } from "../firebase";
@@ -15,8 +15,8 @@ const el = document.getElementById("root");
 Modal.setAppElement(el);
 
 function EditImageModal({ isEditImageOpen, setIsEditImageOpen, book }) {
-  const [images, setImages] = useState([]);
-  const [urls, setUrls] = useState([]);
+  // const [images, setImages] = useState([]);
+  const [images, setImages] = useState(book.images);
   const [isLoading, setIsLoading] = useState(false);
   const [progress, setProgress] = useState(0);
   const history = useHistory();
@@ -25,16 +25,23 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen, book }) {
     history.goBack();
   };
 
-  const onFileChange = async (e) => {
+  const onFileChange = async (event, imageIndex) => {
     setIsLoading(true);
-    for (let i = 0; i < e.target.files.length; i++) {
-      const newImage = e.target.files[i];
-      setImages((prevState) => [...prevState, newImage]);
-      const file = e.target.files[i];
+    for (let i = 0; i < event.target.files.length; i++) {
+      const file = event.target.files[i];
       const storageRef = ref(storage, file.name);
       await uploadBytes(storageRef, file);
-      const l = await getDownloadURL(storageRef);
-      setUrls((prevState) => [...prevState, l]);
+      const uploadedImage = await getDownloadURL(storageRef);
+      const imageURL = images[imageIndex];
+      let newUrl;
+      if (imageURL) {
+        newUrl = uploadedImage;
+      } else {
+        newUrl = uploadedImage;
+      }
+      const newImagesArray = [...images];
+      newImagesArray[imageIndex] = newUrl;
+      setImages(newImagesArray);
     }
     setIsLoading(false);
   };
@@ -42,30 +49,23 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen, book }) {
   const handleAddImages = async (e) => {
     e.preventDefault();
     setIsLoading(true);
-    if (book.images.length > 2) {
-      const copyOfBookImages = [...book.images];
-      urls[0] && copyOfBookImages.splice(0, 1, urls[0]);
-      urls[1] && copyOfBookImages.splice(1, 1, urls[1]);
-      urls[2] && copyOfBookImages.splice(2, 1, urls[2]);
-      const bookRef = doc(db, "books", `${book.id}`);
-      await updateDoc(bookRef, {
-        images: [...copyOfBookImages],
-      });
-    } else {
-      const bookRef = doc(db, "books", `${book.id}`);
-      await updateDoc(bookRef, {
-        images: arrayUnion(...urls),
-      });
-    }
+    const bookRef = doc(db, "books", `${book.id}`);
+    await updateDoc(bookRef, {
+      images: [...images],
+    });
     handleClose();
     setIsLoading(false);
   };
 
   const handleDelete = async (imgIndex) => {
+    setIsLoading(true);
+    const newImagesArray = [...images];
+    setImages(newImagesArray.filter((element) => element !== imgIndex));
     const bookRef = doc(db, "books", `${book.id}`);
     await updateDoc(bookRef, {
       images: arrayRemove(`${imgIndex}`),
     });
+    setIsLoading(false);
   };
 
   if (isLoading) return <Loader />;
@@ -99,7 +99,7 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen, book }) {
               duration: 0.5,
             },
           }}
-          className="flex flex-col justify-center items-center bg-black sm:w-96 rounded-lg p-10 mx-auto relative w-80"
+          className="flex flex-col justify-center items-center bg-black sm:w-96 rounded-lg p-10 mx-auto relative w-80 z-50"
         >
           <CloseButton
             setIsEditImageOpen={setIsEditImageOpen}
@@ -113,21 +113,23 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen, book }) {
               <label className="w-32 text-black flex items-center rounded-md cursor-pointer bg-primary hover:bg-secondary justify-center h-32 mx-auto mb-2">
                 <img
                   src={
-                    urls[0]
-                      ? urls[0]
-                      : book.images[0]
-                      ? book.images[0]
+                    images[0]
+                      ? images[0]
+                      : book.images?.[0]
+                      ? book.images?.[0]
                       : plusIcon
                   }
                   alt="plus icon"
                   className={`${
-                    urls === [] ? "w-10 h-10" : "h-full rounded-md"
+                    images === [] ? "w-10 h-10" : "h-full rounded-md"
                   }`}
                 />
                 <input
                   type="file"
                   name="userPhoto"
-                  onChange={onFileChange}
+                  onChange={(e) => {
+                    onFileChange(e, 0);
+                  }}
                   className="hidden"
                 />
               </label>
@@ -137,12 +139,13 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen, book }) {
                     src={editIcon}
                     alt=""
                     className="w-6 mb-4 rounded-full cursor-pointer"
-                    // onClick={() => handleUpdateImages(book.images.indexOf(book.images[0]))}
                   />
                   <input
                     type="file"
                     name="userPhoto"
-                    onChange={onFileChange}
+                    onChange={(e) => {
+                      onFileChange(e, 0);
+                    }}
                     className="hidden"
                   />
                 </label>
@@ -150,7 +153,7 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen, book }) {
                   src={trashIcon}
                   alt=""
                   className=" cursor-pointer"
-                  onClick={() => handleDelete(book.images[0])}
+                  onClick={() => handleDelete(book.images?.[0])}
                 />
               </div>
             </div>
@@ -158,21 +161,23 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen, book }) {
               <label className="w-32 text-black flex items-center rounded-md cursor-pointer bg-primary hover:bg-secondary justify-center h-32 mx-auto mb-2">
                 <img
                   src={
-                    urls[1]
-                      ? urls[1]
-                      : book.images[1]
-                      ? book.images[1]
+                    images[1]
+                      ? images[1]
+                      : book.images?.[1]
+                      ? book.images?.[1]
                       : plusIcon
                   }
                   alt="plus icon"
                   className={`${
-                    urls === [] ? "w-10 h-10" : "h-full rounded-md"
+                    images === [] ? "w-10 h-10" : "h-full rounded-md"
                   }`}
                 />
                 <input
                   type="file"
                   name="userPhoto"
-                  onChange={onFileChange}
+                  onChange={(e) => {
+                    onFileChange(e, 1);
+                  }}
                   className="hidden"
                 />
               </label>
@@ -182,12 +187,13 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen, book }) {
                     src={editIcon}
                     alt=""
                     className="w-6 mb-4 rounded-full cursor-pointer"
-                    // onClick={() => handleUpdateImages(book.images.indexOf(book.images[1]))}
                   />
                   <input
                     type="file"
                     name="userPhoto"
-                    onChange={onFileChange}
+                    onChange={(e) => {
+                      onFileChange(e, 1);
+                    }}
                     className="hidden"
                   />
                 </label>
@@ -195,7 +201,7 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen, book }) {
                   src={trashIcon}
                   alt=""
                   className=" cursor-pointer"
-                  onClick={() => handleDelete(book.images[1])}
+                  onClick={() => handleDelete(book.images?.[1])}
                 />
               </div>
             </div>
@@ -203,21 +209,23 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen, book }) {
               <label className="w-32 text-black flex items-center rounded-md cursor-pointer bg-primary hover:bg-secondary justify-center h-32 mx-auto mb-2">
                 <img
                   src={
-                    urls[2]
-                      ? urls[2]
-                      : book.images[2]
-                      ? book.images[2]
+                    images[2]
+                      ? images[2]
+                      : book.images?.[2]
+                      ? book.images?.[2]
                       : plusIcon
                   }
                   alt="plus icon"
                   className={`${
-                    urls === [] ? "w-10 h-10" : "h-full rounded-md"
+                    images === [] ? "w-10 h-10" : "h-full rounded-md"
                   }`}
                 />
                 <input
                   type="file"
                   name="userPhoto"
-                  onChange={onFileChange}
+                  onChange={(e) => {
+                    onFileChange(e, 2);
+                  }}
                   className="hidden"
                 />
               </label>
@@ -227,12 +235,13 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen, book }) {
                     src={editIcon}
                     alt=""
                     className="w-6 mb-4 rounded-full cursor-pointer"
-                    // onClick={() => handleUpdateImages(book.images.indexOf(book.images[2]))}
                   />
                   <input
                     type="file"
                     name="userPhoto"
-                    onChange={onFileChange}
+                    onChange={(e) => {
+                      onFileChange(e, 2);
+                    }}
                     className="hidden"
                   />
                 </label>
@@ -240,7 +249,7 @@ function EditImageModal({ isEditImageOpen, setIsEditImageOpen, book }) {
                   src={trashIcon}
                   alt=""
                   className=" cursor-pointer"
-                  onClick={() => handleDelete(book.images[2])}
+                  onClick={() => handleDelete(book.images?.[2])}
                 />
               </div>
             </div>
