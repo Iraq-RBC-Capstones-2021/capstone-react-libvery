f6e386af93d3c96caccbc472281652c546734fbb
Zahraa <alwarda@hotmail.co.uk>
2021-11-03 14:18:48 -0700

    fix a conflict

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ import Loader from "./components/Loader";
 import { AnimatePresence } from "framer-motion";
 import { collection, query, onSnapshot } from "firebase/firestore";
 import { useDispatch } from "react-redux";
-import { fetchBooks } from "./store/booksSlice";
+import { fetchBooks } from "./store/books/booksSlice";
 import { onAuthStateChanged } from "@firebase/auth";
 import { doc, getDoc } from "@firebase/firestore";
 import { auth, db } from "./firebase";
